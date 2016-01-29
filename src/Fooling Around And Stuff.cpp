a3a11216cde725df366353543fe41e7144f5b05d
Chris Beeler <chrisbeeler627@comcast.net>
2016-01-29 12:15:41 -0500

    Work

--- src/Fooling Around And Stuff.cpp
+++ src/Fooling Around And Stuff.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+#include  <iostream>
 using namespace std;
 
 int main(int argc,char**argv)
@@ -22,68 +22,45 @@ int main(int argc,char**argv)
 	string q;
 	string r;
 	string s;
-do {
-	cout << "Welcome to the text adventure! Please have your caps lock on; every answer available will be obvious and fully CAPITALIZED in the text" << endl;
-
-	cout << "What's your first and last name?" << endl;
-
-	cin >> a >> b;
-
-	cout << "Hello " << a << " " << b << endl;
-	cout << "Should I call you by your first name: " << a << ". YES or NO?" << endl;
-
-	cin >> c;
-
-		if (c == "YES") {
-			cout << "okay " << a << ", we're gonna go on an adventure" << endl;
-			n = 0;
-		} else {
-			cout << "Gr8 b8 m8, try again!" << endl;
-			n = 1;
-		}
+		do {
 
 
-	cout << "Where would you like to go? A or B?" << endl;
-	cin >> d;
+			cout << "What's your first and last name?" << endl;
+			cin >> a >> b;
 
-		if (d == "A") {
-			cout << "You come across a mystical dragon, which asks you for the password; Do you want to KILL it or BARGAIN with it?" << endl;
-			cout << "Please choose KILL or BARGAIN" << endl;
-			cin >> e;
-			if (e == "KILL"){
-				cout << "You have a ROCK to your left and a WALL to your right. Which do you choose?" << endl;
-				cin >> f;
+			cout << "Hello " << a << " " << b << endl;
+			cout << "Should I call you by your first name: " << a << ". yes or no?" << endl;
 
-				if (f == "ROCK"){
+			cin >> c;
 
-				} else if (f == "WALL") {
+				if (c == "yes") {
+					cout << "okay " << a << ", we're gonna go on an adventure" << endl;
+					n = 0;
+				} else {
+					cout << "Gr8 b8 m8, try again!" << endl;
+					n = 1;
+				}
 
+		} while (n != 0);
+
+	cout << "Please choose KILL or BARGAIN" << endl;
+				cin >> e;
+				if (e == "KILL"){
+					cout << "You have a ROCK to your left and a WALL to your right. Which do you choose?" << endl;
+					cin >> f;
+
+					if (f == "ROCK"){
+
+					} else if (f == "WALL") {
+
+
+					} else {
+
+					}
+
+				} else if (e == "BARGAIN") {
 
 				} else {
 
 				}
-
-			} else if (e == "BARGAIN") {
-
-			} else {
-
-			}
-
-		}else if (d == "B"){
-
-		}else {
-
-		}
-
-} while (n != 0);
-
-
-
-return 0;
-
-
-}
-
-
-
 
